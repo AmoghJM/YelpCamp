2c5e4adf252de75c9ca992a65e361a5a2488ad31
AmoghJM <149154583+AmoghJM@users.noreply.github.com>
2024-07-26 10:53:35 +0530

    Update showPageMap.js

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,7 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/satellite-streets-v12',
+    style: 'mapbox://styles/mapbox/navigation-night-v1',
     center: campground.geometry.coordinates,
     zoom: 10,
 });
